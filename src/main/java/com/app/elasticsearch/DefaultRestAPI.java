77c3ab98c0d71595d92f0574295aecc44ab8c400
Ashish <chaturvedi.ashish728@gmail.com>
2024-01-24 22:37:49 +0530

    log included

--- src/main/java/com/app/elasticsearch/DefaultRestAPI.java
+++ src/main/java/com/app/elasticsearch/DefaultRestAPI.java
@@ -2,6 +2,8 @@ package com.app.elasticsearch;
 
 import java.time.LocalDateTime;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,14 +11,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class DefaultRestAPI {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(DefaultRestAPI.class);
 
 	@GetMapping("/hello")
 	public String Welcome() {
+		LOG.info("Hello SpringBoot");
 		return "Hello SpringBoot";
 	}
 
 	@GetMapping("/time")
 	public String getTime() {
+		LOG.info(LocalDateTime.now().toString());
 		return LocalDateTime.now().toString();
 	}
 
